ab7997934a62b53c385ca293d4392ea5d51f6834
Luis Guillermo Silva Rojas <guillex@leil.io>
2024-03-25 17:40:11 -0400

    tests: Fix test new mount vs old saunafs (#40)
    It was failing for the following reasons:
    - The previous version uses SAU as EOF marker for the metadata file
    instead of the new SFS.
    - The new admin tool was also expecting an extra item in the 'admin
    info' command.
    - The sfs* tools are deprecated (sfsgetgoal does not exists).
    
    This applies the following fixes:
    - Also check for old signature in the metadata EOF marker.
    - Use saunafs_old_admin_master instead of saunafs_admin_master.
    - Use, for instance, 'saunafsXX saunafs setgoal' instead of 'saunafsXX
    sfssetgoal'.

--- tests/test_suites/ShortSystemTests/test_saunafs_upgrade_new_mount_with_old_saunafs.sh
+++ tests/test_suites/ShortSystemTests/test_saunafs_upgrade_new_mount_with_old_saunafs.sh
@@ -15,13 +15,13 @@ CHUNKSERVERS=2 \
 
 # Start test with master, 2 chunkservers and 2 mounts running legacy SaunaFS code
 # Ensure that we work on legacy version
-assert_equals 1 $(saunafs_admin_master info | grep $SAUNAFSXX_TAG | wc -l)
-assert_equals 2 $(saunafs_admin_master list-chunkservers | grep $SAUNAFSXX_TAG | wc -l)
-assert_equals 2 $(saunafs_admin_master list-mounts | grep $SAUNAFSXX_TAG | wc -l)
+assert_equals 1 $(saunafs_old_admin_master info | grep $SAUNAFSXX_TAG | wc -l)
+assert_equals 2 $(saunafs_old_admin_master list-chunkservers | grep $SAUNAFSXX_TAG | wc -l)
+assert_equals 2 $(saunafs_old_admin_master list-mounts | grep $SAUNAFSXX_TAG | wc -l)
 
 cd "${info[mount0]}"
 mkdir dir0
-assert_success saunafsXX sfssetgoal 2 dir0
+assert_success saunafsXX saunafs setgoal 2 dir0
 cd dir0
 
 function generate_file {
@@ -44,7 +44,7 @@ assert_success file-validate file0
 
 cd ..
 mkdir dir1
-assert_success saunafsXX sfssetgoal 2 dir1
+assert_success saunafsXX saunafs setgoal 2 dir1
 cd dir1
 # Test if reading and writing on new SaunaFS mount works:
 assert_success generate_file file1
