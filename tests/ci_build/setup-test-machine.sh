6b1b49de3fd1ca976a50e227d14c253f0c3dbd3d
Antuan <antuan@leil.io>
2024-04-30 15:44:09 +0200

    tests: Fix long tests to build project
    - Update the repository path.
    - Add disks to setup script for Long Tests that requires 8 disks to
      run.

--- tests/ci_build/setup-test-machine.sh
+++ tests/ci_build/setup-test-machine.sh
@@ -10,12 +10,12 @@ die() {  echo "Error: ${*}" >&2;  exit 1; }
 [ ${UID} -eq 0 ] || die "Run this script as root"
 
 # Run SaunaFS setup script
-mkdir -p /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4
+mkdir -p /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4 /mnt/hdd_5 /mnt/hdd_6 /mnt/hdd_7
 readonly setup_machine_script="${script_dir}/../setup_machine.sh"
 [ -f "${setup_machine_script}" ] || die "Script not found: ${setup_machine_script}"
 grep -v '^[[:space:]]*mount[[:space:]]*[^[:space:]]*$' "${setup_machine_script}" \
-	| bash -x /dev/stdin setup /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4
-chown saunafstest:saunafstest /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4
+	| bash -x /dev/stdin setup /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4 /mnt/hdd_5 /mnt/hdd_6 /mnt/hdd_7
+chown saunafstest:saunafstest /mnt/hdd_0 /mnt/hdd_1 /mnt/hdd_2 /mnt/hdd_3 /mnt/hdd_4 /mnt/hdd_5 /mnt/hdd_6 /mnt/hdd_7
 
 # Extras
 # Install zonefs-tools
