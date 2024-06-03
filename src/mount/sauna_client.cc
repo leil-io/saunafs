997cbb66293694d235eeb53d09a8c937ae83f074
Dave <dave@leil.io>
2024-06-03 00:12:21 -0400

    mount: Apply fixes to DirEntryCache
    Recent tests added to the DirEntryCache batch of unittests revealed some
    bugs in its implementation. One of them consisted in erasing more
    "parent" entries on both lockAndInvalidateParent implementations. The
    other issue was an early stop of the first (and primary) while of the
    lockAndInvalidateParent implementation which considers contexts.
    
    Debugging sessions on failing tests showed issues with files with
    hardlinks, specially caused by the imposibility of knowing the inode of
    the object to remove in the unlink syscall (without extra communication
    with master) which could be used to call lockAndInvalidateInode. Some
    ideas were tried, but the other ones seems to be as bad as any. This one
    was chosen due to it only affects the files with hardlinks, only with
    negative effect it should be asked some extra times its metadata to
    master in sequential queries for data. This seems logical due to the
    previously presented issue.

--- src/mount/sauna_client.cc
+++ src/mount/sauna_client.cc
@@ -838,8 +838,10 @@ EntryParam lookup(Context &ctx, Inode parent, const char *name) {
 		e.attr.st_size=maxfleng;
 	}
 
-	// If lookup succeeded and data did not come from cache, then cache it
-	if (!icacheflag) {
+	// If lookup succeeded and data did not come from cache, then cache it.
+	// Files with at least one hardlink are impossible to keep track of, so it is 
+	// better to don't track them.
+	if (!icacheflag && !(e.attr.st_nlink > 1 && attr[0] == TYPE_FILE)) {
 		auto data_acquire_time = gDirEntryCache.updateTime();
 
 		std::unique_lock<shared_mutex> write_guard(gDirEntryCache.rwlock());
@@ -919,8 +921,10 @@ AttrReply getattr(Context &ctx, Inode ino) {
 	patch_uid_gid_fields(o_stbuf);
 #endif
 
-	// If getattr succeeded and data did not come from cache, then cache it
-	if (!fromCache) {
+	// If lookup succeeded and data did not come from cache, then cache it.
+	// Files with at least one hardlink are impossible to keep track of, so it is 
+	// better to don't track them.
+	if (!fromCache && !(o_stbuf.st_nlink > 1 && attr[0] == TYPE_FILE)) {
 		auto data_acquire_time = gDirEntryCache.updateTime();
 
 		std::unique_lock<shared_mutex> write_guard(gDirEntryCache.rwlock());
