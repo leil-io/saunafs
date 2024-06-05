684d3dde5fbef4abf80e3836422a23bd1dd78cb6
Antuan <antuan@leil.io>
2024-06-05 18:10:18 +0200

    tests: Fix LongSystemTests for building (#100)
    This commit completes the fixes applied at #81 for building SaunaFS.

--- tests/test_suites/LongSystemTests/test_build_while_auto_recovery.sh
+++ tests/test_suites/LongSystemTests/test_build_while_auto_recovery.sh
@@ -2,11 +2,11 @@ timeout_set 1 hour
 assert_program_installed git
 assert_program_installed cmake
 
-CHUNKSERVERS=2 \
+CHUNKSERVERS=3 \
 	MOUNTS=1 \
 	CHUNKSERVER_EXTRA_CONFIG="MASTER_RECONNECTION_DELAY = 1" \
 	MASTER_EXTRA_CONFIG="AUTO_RECOVERY = 1"\
-	MOUNT_EXTRA_CONFIG="sfscachemode=NEVER" \
+	MOUNT_EXTRA_CONFIG="sfscachemode=NEVER|cacheexpirationtime=0" \
 	setup_local_empty_saunafs info
 
 MINIMUM_PARALLEL_JOBS=5
