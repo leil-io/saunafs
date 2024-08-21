40b04c7cc80823d2e77763e2a0dc6da648e97dac
Urmas Rist <urmas@urist.ee>
2024-08-21 12:00:17 +0300

    test: Split tests to smaller, focused suites
    Some tests take more CPU and RAM, some take a lot of space. This commit
    tries to split the tests up, so specific machines and configurations can
    be tailor made for the tests.

--- tests/tools/generate_tests_from_templates.sh
+++ tests/tools/generate_tests_from_templates.sh
@@ -3,15 +3,16 @@
 CMAKE_DIRECTORY="$(pwd)"
 
 for n in {2..21}; do
-	path=${CMAKE_DIRECTORY}/test_suites/LongSystemTests/test_brute_force_lost_chunks_ec_${n}_k.sh
+	suite=${CMAKE_DIRECTORY}/test_suites/BruteForceChunks
+	path=${suite}/test_brute_force_lost_chunks_ec_${n}_k.sh
 	echo "Generating ${path}"
-	cat > ${path} <<-EOF
-	# @generator
-	# callback="@callback@"
-	# source test_suites/TestTemplates/test_brute_force_lost_chunks_ec_n_k.wrapper
-	# @endgenerator
-	n=${n} source test_suites/TestTemplates/test_brute_force_lost_chunks_ec_n_k.inc
-	unset callback
+	cat > ${path} <<- EOF
+		# @generator
+		# callback="@callback@"
+		# source test_suites/TestTemplates/test_brute_force_lost_chunks_ec_n_k.wrapper
+		# @endgenerator
+		n=${n} source test_suites/TestTemplates/test_brute_force_lost_chunks_ec_n_k.inc
+		unset callback
 
-EOF
+	EOF
 done
