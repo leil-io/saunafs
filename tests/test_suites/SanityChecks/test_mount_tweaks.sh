672ba89625a7ebc3b2280c66f241bff288b630c4
Rolando Sánchez Ramos <rolysr@leil.io>
2024-10-31 18:55:21 +0100

    feat(mound): Add ignoreflush to tweaks variables (#228)
    feat(mount): Add ignoreflush to tweaks variables
    
    This change allows the users to modify the sfsignoreflush option during
    runtime execution of the client.

--- tests/test_suites/SanityChecks/test_mount_tweaks.sh
+++ tests/test_suites/SanityChecks/test_mount_tweaks.sh
@@ -7,15 +7,21 @@
 MOUNTS=2 \
   USE_RAMDISK=YES \
   MOUNT_0_EXTRA_CONFIG="sfsdirectio=0" \
-  MOUNT_1_EXTRA_CONFIG="sfsdirectio=1" \
+  MOUNT_1_EXTRA_CONFIG="sfsdirectio=1,sfsignoreflush" \
   setup_local_empty_saunafs info
 
 expect_equals "false" "$(cat ${info[mount0]}/.saunafs_tweaks | grep -i directio | awk '{print $2}')"
+expect_equals "false" "$(cat ${info[mount0]}/.saunafs_tweaks | grep -i ignoreflush | awk '{print $2}')"
 expect_equals "true" "$(cat ${info[mount1]}/.saunafs_tweaks | grep -i directio | awk '{print $2}')"
+expect_equals "true" "$(cat ${info[mount1]}/.saunafs_tweaks | grep -i ignoreflush | awk '{print $2}')"
 
-# Modify the DirectIO option on the fly
+# Modify the options on the fly from tweaks file
 echo "DirectIO=true" | sudo tee ${info[mount0]}/.saunafs_tweaks
+echo "IgnoreFlush=true" | sudo tee ${info[mount0]}/.saunafs_tweaks
 echo "DirectIO=false" | sudo tee ${info[mount1]}/.saunafs_tweaks
+echo "IgnoreFlush=false" | sudo tee ${info[mount1]}/.saunafs_tweaks
 
 expect_equals "true" "$(cat ${info[mount0]}/.saunafs_tweaks | grep -i directio | awk '{print $2}')"
+expect_equals "true" "$(cat ${info[mount0]}/.saunafs_tweaks | grep -i ignoreflush | awk '{print $2}')"
 expect_equals "false" "$(cat ${info[mount1]}/.saunafs_tweaks | grep -i directio | awk '{print $2}')"
+expect_equals "false" "$(cat ${info[mount1]}/.saunafs_tweaks | grep -i ignoreflush | awk '{print $2}')"
