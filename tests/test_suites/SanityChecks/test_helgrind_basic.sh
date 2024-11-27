4f96018ce4c02ca87a57e238b7a755a77b090978
guillex <guillex@leil.io>
2024-11-27 12:26:13 +0100

    test(all): Add new basic helgrind test
    The threading safety was not checked explicitely in our tests.
    Threading issues can cause non-deterministic runtime errors that are
    hard to reproduce and debug. We use multiple threads so far in the
    chunkservers and clients.
    
    This change introduces a basic test using helgrind (from valgrind). The
    idea is to gradually add complexity to this test and create others
    (more specific) when needed.
    
    The initial suppressions file contains placeholders that must be
    classified to determine if they will remain as suppressions or a fix is
    really needed.
    
    The helgrind's history-level was additionally set to ```full``` for
    more accurate catches.

---
+++ tests/test_suites/SanityChecks/test_helgrind_basic.sh
@@ -0,0 +1,54 @@
+timeout_set 45 seconds
+valgrind_enable "helgrind"
+
+CHUNKSERVERS=3 \
+	DISK_PER_CHUNKSERVER=2 \
+	USE_RAMDISK=YES \
+	MASTER_CUSTOM_GOALS="10 ec21: \$ec(2,1)" \
+	# increased timeouts due to expected slowdown by helgrind
+	MOUNT_EXTRA_CONFIG="sfschunkserverwavereadto=5000,`
+		`sfschunkservertotalreadto=20000,maxreadaheadrequests=2,`
+		`cacheexpirationtime=60000" \
+	AUTO_SHADOW_MASTER="NO" \
+	setup_local_empty_saunafs info
+
+number_of_files=4
+file_size=$((1024 * 1024 * 8))
+goals="ec21"
+
+function generateFiles() {
+	for goal in ${goals}; do
+		mkdir ${goal}
+		saunafs setgoal ${goal} ${goal}
+
+		echo "Writing ${number_of_files} files with goal ${goal}"
+		for i in $(seq 1 ${number_of_files}); do
+			FILE_SIZE=${file_size} assert_success file-generate "${goal}/file${i}" &
+		done
+	done
+
+	wait
+
+	echo "Files written"
+}
+
+function validateFiles() {
+	for goal in ${goals}; do
+		echo "Validating ${number_of_files} files with goal ${goal}"
+		for i in $(seq 1 ${number_of_files}); do
+			assert_success file-validate "${goal}/file${i}" &
+		done
+	done
+
+	wait
+
+	echo "Files validated"
+}
+
+cd "${info[mount0]}"
+
+generateFiles
+
+drop_caches
+
+validateFiles
