675de911bb9442320924506b329dae19605f0e0e
Crash <crash@leil.io>
2025-01-24 09:48:12 +0100

    fix(test): Use current branch in the clang test
    The previous test ran always in the dev branch. Therefore, new issues
    were not detected until merged to dev.
    
    This commit updates Clang test to use the version of SaunaFS code from
    the current branch.
    
    Signed-off-by: Crash <crash@leil.io>
    
    Co-authored-by: guillex <guillex@leil.io>

--- tests/test_suites/LongSystemTests/test_build_saunafs_using_clang.sh
+++ tests/test_suites/LongSystemTests/test_build_saunafs_using_clang.sh
@@ -2,26 +2,22 @@ timeout_set 20 minutes
 assert_program_installed git
 assert_program_installed cmake
 assert_program_installed make
-assert_program_installed clang
+assert_program_installed clang++-19
 
 MINIMUM_PARALLEL_JOBS=4
 MAXIMUM_PARALLEL_JOBS=16
 PARALLEL_JOBS=$(get_nproc_clamped_between ${MINIMUM_PARALLEL_JOBS} ${MAXIMUM_PARALLEL_JOBS})
 
-# Clone the repository
-git clone --branch dev https://github.com/leil-io/saunafs.git "${TEMP_DIR}/saunafs"
+# Change to SOURCE_DIR
+cp -r "${SOURCE_DIR}" "${TEMP_DIR}"
+cd "${TEMP_DIR}/${SOURCE_DIR}"
 
-# Change to the temporary directory
-cd "${TEMP_DIR}/saunafs"
+BUILD_DIR=$(mktemp -d)
+chmod 744 "${BUILD_DIR}"
 
-# Check if the feature branch exists and check it out if it does
-feature_branch="build-saunafs-using-clang"
-if git ls-remote --heads origin "${feature_branch}" | \
-	grep -q "${feature_branch}"; then
-	git checkout "${feature_branch}"
-fi
-
-assert_success cmake -B ./build -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+assert_success cmake -B "${BUILD_DIR}" -DCMAKE_BUILD_TYPE=RelWithDebInfo \
 	-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++-19 \
 	-DENABLE_TESTS=ON -DENABLE_NFS_GANESHA=ON -DENABLE_CLIENT_LIB=ON
-assert_success make -C ./build -j${PARALLEL_JOBS}
+assert_success cmake --build "${BUILD_DIR}" -j"${PARALLEL_JOBS}"
+
+rm -rf "${BUILD_DIR}"
